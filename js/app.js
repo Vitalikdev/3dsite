9ac06bc0d5b33b30683cdfd70b02de63c7938421
Vitalikdev <Vitalik.offroad@gmail.com>
2023-06-04 21:41:16 +0300

    add ontouchstart

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
-// document.addEventListener('mousemove', e => {
-//     Object.assign(document.documentElement, {
-//         style: `
-//         --move-x: ${(e.clientX - window.innerWidth / 2) * -0.005}deg;
-//         --move-y: ${(e.clientY - window.innerHeight / 2) * -0.01}deg;
-//         `
-//     })
-// })
+document.addEventListener('mousemove', e => {
+    Object.assign(document.documentElement, {
+        style: `
+        --move-x: ${(e.clientX - window.innerWidth / 2) * -0.005}deg;
+        --move-y: ${(e.clientY - window.innerHeight / 2) * -0.01}deg;
+        `
+    })
+})
 
-document.addEventListener('ontouchmove', e => {
+document.addEventListener('ontouchstart', e => {
     Object.assign(document.documentElement, {
         style: `
         --move-x: ${(e.clientX - window.innerWidth / 2) * -0.005}deg;
